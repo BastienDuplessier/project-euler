3e4b521a69e64b10e090007ff2d0e0ba815ed636
BastienDuplessier <bastien.duplessier@gmail.com>
2013-11-08 14:45:50 +0100

    Solve problem

--- problem1/problem.rb
+++ problem1/problem.rb
@@ -1,3 +1,36 @@
 class Problem1
 
+  def self.under(number)
+    self.new(number)
+  end
+
+  def initialize(max)
+    @max = max
+  end
+
+  def solve
+    list.sum
+  end
+
+  private
+
+  def list
+    list = []
+    (1...@max).each { |number| list << number if (number.divisable?(3,5)) }
+    list
+  end
+end
+
+class Integer
+  def divisable?(*numbers)
+    numbers.any? { |number| (self % number) == 0  }
+  end
+end
+
+class Array
+  def sum
+    result = 0
+    each { |value| result += value }
+    result
+  end
 end
