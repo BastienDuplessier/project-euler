c2a74b0d46a2fb12d791beabccc9b559126b8632
BastienDuplessier <bastien.duplessier@gmail.com>
2013-11-08 14:45:35 +0100

    Edit spec

--- problem1/problem_spec.rb
+++ problem1/problem_spec.rb
@@ -6,11 +6,11 @@ describe "Problem 1" do
   context "Numbers under 10" do
 
     it "should list multiple of 3 and 5" do
-      Problem1.send(:list).should eql([3,5,6,9])
+      Problem1.under(10).send(:list).should eql([3,5,6,9])
     end
 
     it "sum should be 23" do
-      Problem1.solve.should be(23)
+      Problem1.under(10).solve.should be(23)
     end
 
   end
